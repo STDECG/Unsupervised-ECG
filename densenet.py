65ff20c75d1d5dfa56d2f7e4311ec01c979f0de0
STDECG <102400603+STDECG@users.noreply.github.com>
2025-05-07 00:47:14 +0800

    .

--- densenet.py
+++ densenet.py
@@ -80,34 +80,34 @@ import torch.nn as nn
 #         return out
 
 
-class DenseNet(nn.Module):
-    def __init__(self, num_classes=2):
-        super(DenseNet, self).__init__()
-        self.conv1 = nn.Conv1d(1, 16, kernel_size=3, padding=1)
-        self.relu1 = nn.ReLU()
-        self.pool1 = nn.MaxPool1d(2)
-        self.conv2 = nn.Conv1d(16, 32, kernel_size=3, padding=1)
-        self.relu2 = nn.ReLU()
-        self.pool2 = nn.MaxPool1d(2)
-        # 计算经过卷积和池化后的序列长度
-        new_length = 500
-        self.fc1 = nn.Linear(32 * new_length, 64)
-        self.relu3 = nn.ReLU()
-        self.fc2 = nn.Linear(64, num_classes)
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.relu1(x)
-        x = self.pool1(x)
-        x = self.conv2(x)
-        x = self.relu2(x)
-        x = self.pool2(x)
-        x = x.view(x.size(0), -1)
-        x = self.fc1(x)
-        x = self.relu3(x)
-        x = self.fc2(x)
-
-        return x
+# class DenseNet(nn.Module):
+#     def __init__(self, num_classes=2):
+#         super(DenseNet, self).__init__()
+#         self.conv1 = nn.Conv1d(1, 16, kernel_size=3, padding=1)
+#         self.relu1 = nn.ReLU()
+#         self.pool1 = nn.MaxPool1d(2)
+#         self.conv2 = nn.Conv1d(16, 32, kernel_size=3, padding=1)
+#         self.relu2 = nn.ReLU()
+#         self.pool2 = nn.MaxPool1d(2)
+#         # 计算经过卷积和池化后的序列长度
+#         new_length = 500
+#         self.fc1 = nn.Linear(32 * new_length, 64)
+#         self.relu3 = nn.ReLU()
+#         self.fc2 = nn.Linear(64, num_classes)
+#
+#     def forward(self, x):
+#         x = self.conv1(x)
+#         x = self.relu1(x)
+#         x = self.pool1(x)
+#         x = self.conv2(x)
+#         x = self.relu2(x)
+#         x = self.pool2(x)
+#         x = x.view(x.size(0), -1)
+#         x = self.fc1(x)
+#         x = self.relu3(x)
+#         x = self.fc2(x)
+#
+#         return x
 
 
 class DenseNet(nn.Module):
--- predict_shadb.py
+++ predict_shadb.py
@@ -1,11 +1,14 @@
 import torch
-from sklearn.metrics import accuracy_score
+from matplotlib import pyplot as plt
+from sklearn.metrics import accuracy_score, f1_score, recall_score, precision_score, confusion_matrix
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
 import densenet
 from dataset_shdb import ECGDataset
 from utils import set_seed
+
+import seaborn as sns
 
 
 def evaluate(model, val_loader, device):
@@ -33,4 +36,30 @@ if __name__ == '__main__':
 
     test_preds, test_trues = evaluate(model, test_loader, device)
     test_acc = accuracy_score(test_trues, test_preds)
-    print(f'Test Acc: {test_acc}')
+    test_f1 = f1_score(test_trues, test_preds)
+    test_recall = recall_score(test_trues, test_preds)
+    test_precision = precision_score(test_trues, test_preds)
+
+    print(f'Acc: {test_acc}')
+    print(f'F1: {test_f1}')
+    print(f'Recall: {test_recall}')
+    print(f'Precision: {test_precision}')
+
+    # 计算混淆矩阵
+    cm = confusion_matrix(test_trues, test_preds)
+
+    # 定义TP、TN、FP、FN
+    tn, fp, fn, tp = cm.ravel()
+
+    plt.figure(figsize=(8, 6))
+    ax = sns.heatmap(cm, annot=False, fmt='d', cmap='Blues')
+
+    ax.text(0.5, 0.5, f'{tn}\nTN', horizontalalignment='center', verticalalignment='center', color='black')
+    ax.text(1.5, 0.5, f'{fp}\nFP', horizontalalignment='center', verticalalignment='center', color='black')
+    ax.text(0.5, 1.5, f'{fn}\nFN', horizontalalignment='center', verticalalignment='center', color='black')
+    ax.text(1.5, 1.5, f'{tp}\nTP', horizontalalignment='center', verticalalignment='center', color='black')
+
+    plt.xlabel('Predicted Labels')
+    plt.ylabel('True Labels')
+    plt.title('Confusion Matrix')
+    plt.show()
